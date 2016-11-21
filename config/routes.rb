79db52ec5e11a4af51b109ad46432947b523d2b0
Koji <kozzzzy@gmail.com>
2016-11-21 02:11:51 +0000

    新規投稿(new/create)をつけた

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "articles" => "articles#index"
-  get "articles/:id" => "articles#show"
   get "articles/new" => 'articles#new'
+  get "articles/:id" => "articles#show"
+  post "articles" =>  'articles#create'
 end
