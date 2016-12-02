937b48411ae4880818fad586ef89f27c8ea67f53
Koji <kozzzzy@gmail.com>
2016-12-02 07:23:41 +0000

    最終（ルーティングにroot追加）

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   get "articles" => "articles#index"
   get "articles/new" => 'articles#new'
   get "articles/:id" => "articles#show"
   post "articles" =>  'articles#create'
+  
+  root  'articles#index'
 end
